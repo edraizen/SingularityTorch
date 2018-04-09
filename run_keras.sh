b1a12b8e1c527a6eef9006b6ee3d6c74f906b634
Eli Draizen <edraizen@gmail.com>
2018-04-09 14:40:20 -0400

    Create run_keras.sh

---
+++ run_keras.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+
+# Author: Wolfgang Resch (staff@helix.nih.gov)
+
+set -e
+
+keras_img="PATH_TO_SIMG.simg"
+keras_backend="tensorflow"
+cmd="help"
+
+while true; do
+    case "$1" in
+        ipython)
+            cmd="ipython"
+            shift
+            break
+            ;;
+        python)
+            cmd="python"
+            shift
+            break
+            ;;
+        notebook)
+            cmd="notebook"
+            shift
+            break
+            ;;
+        shell)
+            cmd="shell"
+            shift
+            break 
+            ;;
+        *)
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+export SINGULARITY_BINDPATH="/gs2,/gs3,/gs4,/gs5,/gs6,/gs7,/gs8,/gpfs,/spin1,/data,/pdb,/scratch,/fdb,/lscratch"
+export KERAS_BACKEND="$keras_backend"
+export HOSTNAME="cn4216"
+
+module load singularity > /dev/null 2>&1
+
+case "$cmd" in
+    ipython)
+        singularity exec --nv "${keras_img}" ipython "$@"
+        ;;
+    python)
+        singularity exec --nv "${keras_img}" /anaconda/bin/python "$@"
+        ;;
+    notebook)
+        singularity exec --nv "${keras_img}" jupyter notebook "$@" 
+        ;;
+    shell)
+        singularity shell "${keras_img}" "$@"
+        ;;
+    help)
+        usage
+        exit 0
+        ;;
+esac
