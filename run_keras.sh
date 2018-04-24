4e102f3f5e41780c9e004bce7a3bebf13d6846b3
Eli Draizen <edraizen@gmail.com>
2018-04-24 11:10:48 -0400

    Update run_keras.sh

--- run_keras.sh
+++ run_keras.sh
@@ -37,7 +37,7 @@ while true; do
     esac
 done
 
-export SINGULARITY_BINDPATH="/gs2,/gs3,/gs4,/gs5,/gs6,/gs7,/gs8,/gpfs,/spin1,/data,/pdb,/scratch,/fdb,/lscratch"
+export SINGULARITY_BINDPATH="/gs3,/gs4,/gs5,/gs6,/gs7,/gs8,/gs11,/gpfs,/spin1,/data,/pdb,/scratch,/fdb,/lscratch"
 export KERAS_BACKEND="$keras_backend"
 export HOSTNAME="cn4216"
 
